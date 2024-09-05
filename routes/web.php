47278423d926c0ea058e08647f127e8559049072
Kevin-Poli <khususmaen4154@gmail.com>
2024-09-05 17:08:02 +0700

    Update Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +20,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+Route::get('/world',function (){
+    return 'World';
 });
+
+Route::get('/selamat',function (){
+    return 'Selamat Datang';
+});
+
+// Route::get('/about',function (){
+//     return '2241760125 Kevin Arullah Herdiansyah';
+// });
+
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama Saya '.$name; 
+    }); 
+
+Route::get('/posts/{post}/comments/{comments}',function ($postId,$commentId){
+return 'Pos ke-'.$postId."Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{articles}/id/{id}' ,function($id,$articles){
+    return 'Halaman Artikel ke - '.$articles."Id ke - ".$id;
+});
+
+// Route::get('/user/{name?}',function($name=null){
+//     return 'Nama Saya'.$name;
+// });
+
+Route::get('/user/{name?}',function($name='mpin'){
+    return 'Nama Saya '.$name;
+});
+
+Route::get('hello', [WelcomeController::class,'hello']);
+
+Route::get('index', [HomeController::class,'index']);
+Route::get('about', [AboutController::class,'about']);
+Route::get('articles/id/{id}', [ArticleController::class,'dinamis']);
+
+Route::resource('photos', PhotoController::class)->only([ 'index', 'show' ]); 
+Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy' ]);
+
+// Route::get('/greeting',function(){
+//     return view('blog.hello',['name'=>'Mpin']);
+// });
+
+Route::get('/greeting',[WelcomeController::class,'greeting']);
